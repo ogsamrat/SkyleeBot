bf227171bd9d6bc382b001a2a5aad036b78ad0fd
StarkGang <StarkGangZ@gmail.com>
2020-08-06 10:01:59 +0530

    Update misc.py

--- skylee/modules/misc.py
+++ skylee/modules/misc.py
@@ -341,7 +341,7 @@ def ud(update, context):
 @typing_action
 def src(update, context):
     update.effective_message.reply_text(
-        "Hey there! You can find what makes me click [here](www.github.com/starry69/skyleebot).",
+        "Hey there! You can find what makes me click [Here](t.me/meikoSupport) .",
         parse_mode=ParseMode.MARKDOWN,
         disable_web_page_preview=True,
     )
